3af2335a45acd2307b120557430cbed968c15646
Jack Cook <hello@jackcook.nyc>
2015-03-14 19:03:49 -0400

    Implemented hourly weather visuals

--- Mist/MainViewController.swift
+++ Mist/MainViewController.swift
@@ -11,12 +11,19 @@ import CoreLocation
 import SwiftyJSON
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet var imageView: UIImageView!
+    @IBOutlet var tableView: UITableView!
+    
+    @IBOutlet var bottomView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.backgroundColor = bottomView.backgroundColor
         
         Places.autocomplete("big sky", completion: { (data, error) -> Void in
             let json = JSON(data: data)
@@ -29,14 +36,28 @@ class MainViewController: UIViewController {
 //            println(forecast.current.temperature)
         })
         
-        newPhoto()
-        let timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "newPhoto", userInfo: nil, repeats: true)
-        NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
+//        newPhoto()
+//        let timer = NSTimer.scheduledTimerWithTimeInterval(5, target: self, selector: "newPhoto", userInfo: nil, repeats: true)
+//        NSRunLoop.mainRunLoop().addTimer(timer, forMode: NSRunLoopCommonModes)
     }
     
     func newPhoto() {
         Google.getImage(40.720032, lon: -73.988354) { (image) -> Void in
             self.imageView.image = image
         }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("ForecastCell", forIndexPath: indexPath) as UITableViewCell
+        
+        return cell
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
     }
 }
