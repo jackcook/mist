88e28dea0b7d208de8ec54d1b062957d84fcf881
Jack Cook <jack@jackcook.nyc>
2015-03-17 16:07:45 -0400

    Create WatchKit app/extension

---
+++ Mist WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Mist WatchKit Extension
+//
+//  Created by Jack Cook on 3/17/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Mist WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Mist WatchKit Extension
+//
+//  Created by Jack Cook on 3/17/15.
+//  Copyright (c) 2015 Jack Cook. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
